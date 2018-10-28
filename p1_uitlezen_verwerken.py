fa24ed48451f37c557941f9b089db555dec8fdd7
geertk94 <pukcon-2tyNfi-budtyj>
2018-10-28 12:15:04 +0100

    added serial config

---
+++ p1_uitlezen_verwerken.py
@@ -0,0 +1,19 @@
+import sys
+import serial
+
+#Set COM port config
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.bytesize=serial.EIGHTBITS
+ser.parity=serial.PARITY_NONE
+ser.stopbits=serial.STOPBITS_ONE
+ser.xonxoff=0
+ser.rtscts=0
+ser.timeout=20
+ser.port="/dev/ttyUSB0"
+
+#Open COM port
+try:
+    ser.open()
+except:
+    sys.exit ("Fout bij het openen van %s. Programma afgebroken."  % ser.name)
