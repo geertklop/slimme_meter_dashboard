3f312e2da0c14674135fae0db760d3162f8582d1
geertk94 <pukcon-2tyNfi-budtyj>
2018-11-03 14:25:30 +0100

    added sqlite into script

--- p1_uitlezen_verwerken.py
+++ p1_uitlezen_verwerken.py
@@ -1,20 +1,48 @@
 import sys
 import serial
 import re
+import datetime as dt
+import sqlite3
+
 from telegram_functions import *
 
 list_of_interesting_codes = {
     '1-0:1.8.1': 'total_received_1',
     '1-0:1.8.2': 'total_received_2',
-    '1-0:2.8.1': 'total_deliverd_1',
-    '1-0:2.8.2': 'total_deliverd_2',
+    '1-0:2.8.1': 'total_delivered_1',
+    '1-0:2.8.2': 'total_delivered_2',
 }
 
+#get current date/time
+date = dt.datetime.now()
+
+# get current reading
 raw_telegram = read_telegram()
 telegram_dict = telegram_to_dict(raw_telegram)
 values_dict = extract_interesting_codes(telegram_dict,
                                         list_of_interesting_codes
                                         )
 
-for key in values_dict.keys():
-    print(key, ':', values_dict[key])
+verbruik1 = values_dict['1-0:1.8.1']
+verbruik2 = values_dict['1-0:1.8.2']
+terug1 = values_dict['1-0:2.8.1']
+terug2 = values_dict['1-0:2.8.2']
+verbruik_delta = 0
+terug_delta = 0
+
+#connect to db
+db = sqlite3.connect('../data/meterdata.db')
+cursor = db.cursor()
+
+# insert data
+cursor.execute('''INSERT INTO meterstanden(currentdate, verbruik1, verbruik2, terug1, terug2, verbruik_delta, terug_delta)
+                  VALUES(?,?,?,?,?,?,?)''', (currentdate,
+                                             verbruik1,
+                                             verbruik2,
+                                             terug1,
+                                             terug2,
+                                             verbruik_delta,
+                                             terug_delta))
+db.commit()
+
+
