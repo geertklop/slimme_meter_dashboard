45ccdd28bbed4a85fd0302c1968449cc6c23c0ca
geertk94 <pukcon-2tyNfi-budtyj>
2018-11-04 14:26:11 +0100

    added sqlite into script

---
+++ airflow_dag.py
@@ -0,0 +1,12 @@
+import datetime as dt
+import sqlite3
+
+from reader_functions.telegram_functions import *
+from reader_functions.p1_uitlezen_verwerken import *
+
+def read_extract_save():
+    currentdate, verbruik1, verbruik2, verbruik2, terug1, terug2 = read_and_extract_data()
+
+    data_to_sqlite()
+
+    return 'succes'
