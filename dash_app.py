07718d8f6aef03e63507f852410202ab44c4ae70
geertk94 <pukcon-2tyNfi-budtyj>
2018-11-04 21:14:47 +0100

    created basic Dash app

--- dash_app.py
+++ dash_app.py
@@ -46,7 +46,7 @@ def update_daily_total(n):
     db = sqlite3.connect('/home/gklop/slimme_meter_project/data/meterdata.db')
     df = pd.read_sql_query("select * from meterstanden where currentdate >= '{}';".format(first_of_month), db)
 
-    df['currentdate'] = pd.to_datetime(df['currentdate'])
+    df['currentdate'] = pd.to_datetime(df['currentdate']).dt.date
     print(df.dtypes)
     print(df.currentdate)
 
