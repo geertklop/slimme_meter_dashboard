3970a8c05b0bdd3ab5a4c353e8865c572b17e645
geertk94 <pukcon-2tyNfi-budtyj>
2018-11-04 16:21:05 +0100

    created basic Dash app

--- dash_app.py
+++ dash_app.py
@@ -1,8 +1,69 @@
 import pandas as pd
+import datetime
 import sqlite3
 import dash
-import dash-core-components
-import dash-html-components
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.Layout = html.Div(
+    html.Div([
+        html.H2('Energieverbuik per 5 minuten'),
+        dcc.Graph(id='live-update-graph'),
+        dcc.Interval(
+            id='interval-component',
+            interval = 5000,
+            n_interval = 0
+        )
+    ])
+)
+
+@app.callback(Output('live-update-text', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph(n):
+    current_year = datetime.datetime.now().month
+    current_month = datetime.datetime.now().month
+
+    first_of_month = datetime.datetime(current_year,
+                                       current_month,
+                                       1
+                                       )
+    # fetch data from database
+    db = sqlite3.connect('/home/gklop/slimme_meter_project/data/meterdata.db')
+    df = pd.read_sql_query("select * from meterstanden where currentdate >= {};".format(first_of_month), db)
+
+    traces = []
+    for i in [verbruik_delta, terug_delta]:
+        traces.append(go.Scatter(
+            x = df['currentdate'],
+            y = df[i],
+            text = df[i],
+            mode='lines'
+        ))
+
+    return {'data': traces,
+            'layout': go.Layout(
+                xaxis=dict(title=''),
+                yaxis=dict(title='verbruik in kWh'),
+                hovermode='closest'
+            )
+    }
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0'
+                   )
+
+
+
+
+
+
+
 
 
 
