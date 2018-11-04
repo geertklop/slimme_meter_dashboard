a6074d14567d80f5391fb1c9d61793c5b0141c59
geertk94 <pukcon-2tyNfi-budtyj>
2018-11-04 20:16:25 +0100

    created basic Dash app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # slimme_meter_dashboard
+
+Telegram Functions: Open Serial port and extract interesting variables.
+p1_uitlezen_verwerken: Extract interesting variables to values and store in sqlite database.
+
+Ubuntu/startup processes made in /etc/systemd/system/
--- p1_uitlezen_verwerken.py
+++ p1_uitlezen_verwerken.py
@@ -16,60 +16,63 @@ list_of_interesting_codes = {
 }
 def main_loop():
     while True:
-        start_time = time.time()
-        currentdate = dt.datetime.now()
+        try:
+            start_time = time.time()
+            currentdate = dt.datetime.now()
 
-        # get current reading
-        raw_telegram = read_telegram()
-        telegram_dict = telegram_to_dict(raw_telegram)
-        values_dict = extract_interesting_codes(telegram_dict,
-                                                list_of_interesting_codes
-                                                )
+            # get current reading
+            raw_telegram = read_telegram()
+            telegram_dict = telegram_to_dict(raw_telegram)
+            values_dict = extract_interesting_codes(telegram_dict,
+                                                    list_of_interesting_codes
+                                                    )
 
-        verbruik1 = values_dict['1-0:1.8.1']
-        verbruik2 = values_dict['1-0:1.8.2']
-        terug1 = values_dict['1-0:2.8.1']
-        terug2 = values_dict['1-0:2.8.2']
+            verbruik1 = values_dict['1-0:1.8.1']
+            verbruik2 = values_dict['1-0:1.8.2']
+            terug1 = values_dict['1-0:2.8.1']
+            terug2 = values_dict['1-0:2.8.2']
 
-        #connect to db
-        db = sqlite3.connect('/home/gklop/slimme_meter_project/data/meterdata.db')
-        cursor = db.cursor()
+            #connect to db
+            db = sqlite3.connect('/home/gklop/slimme_meter_project/data/meterdata.db')
+            cursor = db.cursor()
 
-        # select last row
-        cursor.execute('''SELECT * FROM meterstanden ORDER BY id DESC LIMIT 1''')
-        for row in cursor:
-            verbruik1_last = row[2]
-            verbruik2_last = row[3]
-            terug1_last = row[4]
-            terug2_last = row[5]
+            # select last row
+            cursor.execute('''SELECT * FROM meterstanden ORDER BY id DESC LIMIT 1''')
+            for row in cursor:
+                verbruik1_last = row[2]
+                verbruik2_last = row[3]
+                terug1_last = row[4]
+                terug2_last = row[5]
 
-        # calculate deltas
-        verbruik_delta = (verbruik1 - verbruik1_last) + (verbruik2 - verbruik2_last)
-        terug_delta = (terug1 - terug1_last) + (terug2 - terug2_last)
+            # calculate deltas
+            verbruik_delta = (verbruik1 - verbruik1_last) + (verbruik2 - verbruik2_last)
+            terug_delta = (terug1 - terug1_last) + (terug2 - terug2_last)
 
-        # insert new data into table
-        query = """
-                INSERT INTO meterstanden(currentdate, verbruik1, verbruik2, terug1, terug2, verbruik_delta, terug_delta)
-                VALUES(?,?,?,?,?,?,?)
-                """
+            # insert new data into table
+            query = """
+                    INSERT INTO meterstanden(currentdate, verbruik1, verbruik2, terug1, terug2, verbruik_delta, terug_delta)
+                    VALUES(?,?,?,?,?,?,?)
+                    """
 
-        cursor.execute(query, (currentdate,
-                               verbruik1,
-                               verbruik2,
-                               terug1,
-                               terug2,
-                               verbruik_delta,
-                               terug_delta))
+            cursor.execute(query, (currentdate,
+                                   verbruik1,
+                                   verbruik2,
+                                   terug1,
+                                   terug2,
+                                   verbruik_delta,
+                                   terug_delta))
 
-        db.commit()
-        db.close()
+            db.commit()
+            db.close()
 
-        end_time = time.time()
-        run_time = end_time - start_time
-        # print(currentdate, run_time, 'successful run')
+            end_time = time.time()
+            run_time = end_time - start_time
+            # print(currentdate, run_time, 'successful run')
 
 
-        time.sleep(300 - run_time)
+            time.sleep(300 - run_time)
+        except KeyError:
+            continue
 
 if __name__ == '__main__':
     try:
