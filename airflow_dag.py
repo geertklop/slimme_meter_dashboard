25dcb3e92d15d25ff311efe0d0faab5bea2be524
geertk94 <pukcon-2tyNfi-budtyj>
2018-11-04 14:27:49 +0100

    added sqlite into script

--- airflow_dag.py
+++ airflow_dag.py
@@ -7,6 +7,6 @@ from reader_functions.p1_uitlezen_verwerken import *
 def read_extract_save():
     currentdate, verbruik1, verbruik2, verbruik2, terug1, terug2 = read_and_extract_data()
 
-    data_to_sqlite()
+    data_to_sqlite(currentdate, verbruik1, verbruik2, verbruik2, terug1, terug2)
 
     return 'succes'
